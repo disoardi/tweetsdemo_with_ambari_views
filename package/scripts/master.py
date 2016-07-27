577f4dcb3867089d86158e953be93ab4dc7f6c99
disoardi <isoardi@ecubecenter.it>
2016-07-27 12:55:50 +0200

    correct sed for kiwi dashboard

--- package/scripts/master.py
+++ package/scripts/master.py
@@ -84,7 +84,7 @@ class Master(Script):
 
     Execute('sed -i "s/iFrame View/Kiwi dashboard/g" /opt/iframe-view/src/main/resources/view.xml')
     Execute('sed -i "s/IFRAME_VIEW/KIWI_DASHBOARD/g" /opt/iframe-view/src/main/resources/view.xml')
-    Execute('sed -i "s/6080/8983\/solr\/kiwi\/index.html/g" /opt/iframe-view/src/main/resources/index.html')
+    Execute('sed -i "s/banana/kiwi/g" /opt/iframe-view/src/main/resources/index.html')
     Execute('sed -i "s/iframe-view/kiwi_dashboard-view/g" /opt/iframe-view/pom.xml')
     Execute('sed -i "s/Ambari iFrame View/Kiwi dashboard view/g" /opt/iframe-view/pom.xml')
     Execute('rm -rf /opt/iframe-view/target')
